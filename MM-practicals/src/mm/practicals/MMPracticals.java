48e32d20a0eecb34860875c581bbbc547ddff2bb
27226018@HeinKruger <48180500+HeinKruger@users.noreply.github.com>
2019-05-10 23:42:27 +0200

    upload

---
+++ MM-practicals/src/mm/practicals/MMPracticals.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mm.practicals;
+
+/**
+ *
+ * @author Hein
+ */
+public class MMPracticals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
