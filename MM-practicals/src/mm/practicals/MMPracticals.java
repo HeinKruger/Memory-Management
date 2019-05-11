ab0b87f89316137e9d4485d0cc64eee85a645060
HeinKruger <27226018@student.g.nwu.ac.za>
2019-05-11 09:27:22 +0200

    initial
    initial

--- MM-practicals/src/mm/practicals/MMPracticals.java
+++ MM-practicals/src/mm/practicals/MMPracticals.java
@@ -16,6 +16,9 @@ public class MMPracticals {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("all processes in memory:");
+        
     }
     
 }
